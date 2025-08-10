d339fa552be4eb2d7716888f6b623547c9f7b441
gladsonsimoes <gladsonssimoes@outlook.com.br>
2025-08-10 15:58:21 -0300

    ..

--- src/script.ps1
+++ src/script.ps1
@@ -1,44 +1,57 @@
-Write-Host "Escolha uma op��o:"
+$downloadimg = ".\1.download_img_links.ps1"
+$convertPdf = ".\2.image-for-pdf-OCR.ps1"
+$pdfMerge = ".\3.pdf-merged.ps1"
+$compressedPdf = ".\4.pdf-comprimed.ps1"
+
+
+Write-Host "Escolha uma opcao:"
 Write-Host "1 - Baixar imagem do arquivo html"
 
-$opcao = Read-Host "Digite o n�mero da op��o"
+$opcao = Read-Host "Digite o numero da opcao"
 
-if ($opcao -eq "1") {
+if ($opcao -eq "1") 
+{
     Write-Host "Baixando imagens dos links do arquivo html..."
-    & ".\scr\download_img_links.ps1"
+    & $downloadimg
 
     Write-Host "Deseja converter as imagens para PDF? "
     Write-Host "1 - converter com o modo OCR"
-    Write-Host "2 - converter sem o modo OCR"
+    
 
     $opcao = Read-Host "Digite o n�mero da op��o:"
 
-    if ($opcao -eq "1") {
+    if ($opcao -eq "1") 
+    {
        Write-Host "Baixando imagens dos links do arquivo html..."
-       & ".\scr\Com OCR\image-for-pdf-OCR.ps1"
+       & $convertPdf
 
        Write-Host "Deseja mesclar os pdf gerado? "
        Write-Host "1 - Mesclar PDFS"
 
-        $opcao = Read-Host "Digite o n�mero da op��o:"
+        $opcao = Read-Host "Digite o numero invalida:"
 
         if($opcao -eq "1"){
             Write-Host "Mesclando"
-            & ".\scr\Com OCR\pdf-merged.ps1"
+            & $pdfMerge
+            
+            Write-Host "Deseja reduzir o tamanho do arquivo"
+            Write-Host "1 - reduzir tamanho do PDF"
+
+            $opcao = Read-Host "Digite o numero da opcao"
+
+            if ($opcao -eq "1") {
+            Write-Host "Reduzindo o tamanho do arquivo" 
+            & $compressedPdf
+            } else {
+               exit
+            } 
+
         } else {
-            Write-Host "Op��o inv�lida!"
+            Write-Host "Opcao invalida!"
+        }
     }
-
-} elseif ($opcao -eq "2") {
-    Write-Host "Executando sem o modo OCR..."
-    & ".\scr\Sem OCR\image-merged-pdf.ps1"
-} else {
-    Write-Host "Op��o inv�lida!"
-}
-
-
-
-} else {
+} 
+else {
     Write-Host "Op��o inv�lida!"
 }
 
